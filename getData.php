f54b7f0ab10c6c69b65eeb2c8d24dd6ad460afad
hongily25 <hlam93@gmail.com>
2014-11-19 00:02:50 -0800

    Update getData.php

--- getData.php
+++ getData.php
@@ -3,7 +3,7 @@
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
-$string = file_get_contents("convertcsv.json");
+$string = file_get_contents("https://github.com/hongily25/emilylam/blob/master/convertcsv.json");
 echo $string;
 
 // Instead you can query your database and parse into JSON etc etc
