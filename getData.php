39a744169fc0bef8da9e9cfab5ae62778496ed61
Emily Lam <hlam93@gmail.com>
2014-11-19 10:52:14 -0800

    literal javascript obj

--- getData.php
+++ getData.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
-$string = file_get_contents("https://github.com/hongily25/emilylam/blob/master/convertcsv.json");
+$string = file_get_contents("convertcsv.json");
 echo $string;
 
 // Instead you can query your database and parse into JSON etc etc
