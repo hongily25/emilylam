517c82cf544d93a6afc55bd5ef3e4f4bef68f598
Emily Lam <hlam93@gmail.com>
2014-11-23 02:19:18 -0800

    console log

--- getData.php
+++ getData.php
@@ -1,8 +1,5 @@
 <?php 
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
