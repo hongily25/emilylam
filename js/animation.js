c89eceffa52d05b5fcc05230c80ade8e798ea153
Emily Lam <hlam93@gmail.com>
2015-03-22 12:29:02 -0700

    Revert 3b9bb77..ccc26d1
    This rolls back to commit 3b9bb77d9d1d2ecac034cae612affd10b58b3219.

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,28 @@
-window.onload = function () {
+window.onload = function(){
+	
+	
+ /*TweenLite.to(".test", 10, {left:600}); 
+	
+	var controller = new ScrollMagic();
 
+	var tween = TweenMax.fromTo(".slytherin", 1,
+								{left: -1000},
+								{left: 0, ease: Circ.easeInOut}
+							);
 
-		$(function () {
-			// Init Controller
-			var s = skrollr.init();
+	// build scene
+	var scene = new ScrollMagic.Scene(triggerElement: "#sect2")
+					.setTween(tween)
+					.addIndicators() 
+					.addTo(controller); */
+	
+					var scene = new ScrollMagic.Scene({
+									triggerElement: "#slytherin"
+								})
+								.setTween("#blah", 0.5, {backgroundColor: "green", scale: 2.5}) // trigger a TweenMax.to tween
+								.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+								.addTo(controller);
+	
+	
+}
 
-		});
-
-		};
